b7a76612252f6403920a62655d27966ca41013f7
Chua Zi Heng <56360597+chuaziheng@users.noreply.github.com>
2021-09-15 01:42:12 +0800

    Delete include directory

--- include/openpose/calibration/headers.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef OPENPOSE_CALIBRATION_HEADERS_HPP
-#define OPENPOSE_CALIBRATION_HEADERS_HPP
-
-// calibration module
-#include <openpose/calibration/cameraParameterEstimation.hpp>
-
-#endif // OPENPOSE_CALIBRATION_HEADERS_HPP
